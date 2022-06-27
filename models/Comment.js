63d3d3fb4b0424976ac3cf8b0f0874da3fb92524
Theresa <tab61295@gmail.com>
2022-06-27 17:18:47 -0500

    add comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,18 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Comment extends Model {}
+class Comment extends Model { }
 
 Comment.init(
-  {
-    // columns will go here
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'comment'
-  }
+    {
+        // columns will go here
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
 );
 
 module.exports = Comment;
